966f41c1f3621e5576add7d55ffe566e6d30f083
priyankraychura <priyankraychura@gmail.com>
2024-11-20 17:13:59 +0530

    bubbleSort completed

---
+++ DAY-5/bubbleSort.js
@@ -0,0 +1,19 @@
+let arr = [102, 12, 45, 6, 23, 13, 67, 33, 99]
+let sorted = false;
+
+for(let i=0; i<arr.length; i++){
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i] > arr[i+1]){
+            let temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+            sorted = true;
+        }
+    }
+
+    if(sorted == false){
+        break;
+    }
+}
+
+console.log(arr)
