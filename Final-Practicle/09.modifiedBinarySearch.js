436bb07528f2c4c65a836e303a5d8ba2754f127a
priyankraychura <priyankraychura@gmail.com>
2024-12-23 18:14:22 +0530

    issues resolved

--- Final-Practicle/09.modifiedBinarySearch.js
+++ Final-Practicle/09.modifiedBinarySearch.js
@@ -1,19 +1,34 @@
-let arr = [4, 5, 6, 7, 0, 1, 2];
-const target = 3;
-let start = 0;
-let end = arr.length - 1;
+let arr =  [4, 5, 6, 7, 0, 1, 2]
+target = 0  
+left = 0
+right = arr.length - 1
 
-while(start <= end){
-    let mid = Math.floor((start + end)/2);
+function rotatearr(){
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2)
+        
+        if(arr[mid] == target){
+            return mid
+        }
 
-    if(arr[mid] == target){
-        console.log(mid);
-        break;
+        if(arr[left] <= arr[mid]){
+            if(target >= arr[left] && target <= arr[mid]){
+                right = mid - 1
+            }
+            else{
+                left = mid + 1
+            }
+        }
+        else{
+            if(target > arr[mid] && target <= arr[right]){
+                left = mid + 1
+            }
+            else{
+                right = mid - 1
+            }
+        }
     }
-
-    if(target < arr[mid]){
-        end = mid - 1;
-    } else if(target > arr[mid]){
-        start = mid + 1;
-    }
+    return -1
 }
+
+console.log(rotatearr())
